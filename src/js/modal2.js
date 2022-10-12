9968239883cc3471b1203762d2d0bee593af02a9
Max Gerasymenko <geramaks03@gmail.com>
2022-10-12 16:51:32 +0300

    Update modal2.js

--- src/js/modal2.js
+++ src/js/modal2.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal2-open]"),
+    openModalBtn2: document.querySelector("[data-modal2-open2]"),
+    openModalBtn3: document.querySelector("[data-modal2-open3]"),
     closeModalBtn: document.querySelector("[data-modal2-close]"),
     modal: document.querySelector("[data-modal2]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
